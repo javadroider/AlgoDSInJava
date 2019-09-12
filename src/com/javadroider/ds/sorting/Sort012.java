dcb7b8ef5dd9b06836c135d068644a982fcd8861
javadroider <javadroider@gmail.com>
2019-09-12 12:44:20 +0000

    refactoring

--- src/com/javadroider/ds/sorting/Sort012.java
+++ src/com/javadroider/ds/sorting/Sort012.java
@@ -7,6 +7,14 @@ public class Sort012 {
 
         int a[] = {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1};
         int arr_size = a.length;
+
+        int array[] = {1, 2, 3, 4, 5, 6, 7, 9};
+        int x = 0, y = 0;
+
+       //option1(a, arr_size);
+    }
+
+    private static void option1(int[] a, int arr_size) {
         int lo = 0;
         int hi = arr_size - 1;
         int mid = 0, temp = 0;
